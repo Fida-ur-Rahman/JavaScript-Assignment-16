53c0eb3d22b76f7c7f2d3eef3f16781c9b9c1abf
Fida-ur-Rahman <fidasaba15@gmail.com>
2026-01-01 23:10:46 +0500

    JavaScript Assignment No 16

---
+++ DOM1.js
@@ -0,0 +1,16 @@
+// THE DOM FULL FORM DOMCUMENT OBJECT MODEL
+let heading = document.getElementById("heading")// h1
+console.dir(heading)
+
+let heading2 = document.getElementsByClassName("heading")// h2
+console.dir(heading2)
+
+let heading23 = document.getElementsByTagName("heading")// p
+console.dir(heading23)
+
+let element = document.querySelector('p')
+console.log(element)
+
+let element2 = document.querySelectorAll('p')
+console.log(element2)
+
